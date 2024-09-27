091a5fa896e72c012f5237b562ab375da1ec3226
Catherine Mei <meic1212@mit.edu>
2024-09-26 23:27:47 -0400

    removed to only prototype one relevant

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import type { Component } from "solid-js";
+import { TraversalOutputComponentKeyboardParentFocus } from "./priority-traversal-parent-focus";
+
+import { stackedBarChartHypergraph } from "./input-data";
 
 const App: Component = () => {
   const path = window.location.pathname;
 
   const pathPrefix = path.includes("benthic") ? "/benthic" : "";
   return (
-    <div>
-      <h1>Main App</h1>
-      <p>Select a prototype to view:</p>
-      <a href={`${pathPrefix}/proto-one`}>Go to Prototype 1</a>
-      <br />
-      <a href={`${pathPrefix}/proto-two`}>Go to Prototype 2</a>
-    </div>
+    <TraversalOutputComponentKeyboardParentFocus
+      nodeGraph={stackedBarChartHypergraph}
+      showHypergraph={false}
+    />
   );
 };
 
